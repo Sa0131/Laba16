782d360f636cb2a84b895b186a220e546312a072
Sa0131 <novruz24082015@gmail.com>
2022-12-20 22:12:30 +0300

    Initial commit

---
+++ src/ru/mirea/lab161/Main.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab161;
+
+public class Main {
+    public static void main(String[] args) {
+        Form1 form = new Form1();
+        form.setVisible(true);
+
+        Form2 form2 = new Form2();
+        form2.setVisible(true);
+
+        Form3 form3 = new Form3();
+        form3.setVisible(true);
+    }
+}
